af4704f04fcbe531625286643fca7484bce3cc1c
Freddy Zambrano <fredd.fazm@gmail.com>
2025-02-20 23:48:43 -0400

    Re add S3 Bucket

--- app.py
+++ app.py
@@ -3,13 +3,26 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
+import boto3
+from botocore.exceptions import ClientError
 from models import db, Realtor, Property, PropertyPhoto
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u2cch0htlm8mg2:p79a5b6c1cdc7e523a1978c34d01c651f82ebb1c599d9e3b0dd95a0e27898d8cf@c9mq4861d16jlm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d32ncghf1gn534'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# S3 Configuration
+S3_BUCKET = os.environ.get('AWS_BUCKET_NAME')
+S3_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+S3_SECRET = os.environ.get('AWS_SECRET_ACCESS_KEY')
+S3_LOCATION = f'https://{S3_BUCKET}.s3.amazonaws.com/'
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
+)
 
 db.init_app(app)
 login_manager = LoginManager(app)
@@ -77,60 +90,62 @@ def properties():
     properties = Property.query.filter_by(realtor_id=current_user.id).all()
     return render_template('properties.html', properties=properties)
 
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+    try:
+        filename = secure_filename(file.filename)
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+        return f"{S3_LOCATION}{filename}"
+    except Exception as e:
+        print("Something Happened: ", e)
+        return None
 
-
-
-def upload_file_to_s3(file, filename):
-      try:
-          s3.upload_fileobj(
-              file,
-              BUCKET_NAME,
-              filename,
-              ExtraArgs={'ACL': 'public-read'}
-          )
-          return f"https://{BUCKET_NAME}.s3.amazonaws.com/{filename}"
-      except ClientError as e:
-          return None
 @app.route('/properties/add', methods=['GET', 'POST'])
 @login_required
 def add_property():
-      if request.method == 'POST':
-          property = Property(
-              realtor_id=current_user.id,
-              title=request.form.get('title'),
-              description=request.form.get('description'),
-              location=request.form.get('location'),
-              price=float(request.form.get('price')),
-              property_type=request.form.get('property_type'),
-              area=float(request.form.get('area')),
-              bedrooms=int(request.form.get('bedrooms')),
-              bathrooms=int(request.form.get('bathrooms')),
-              status='available'
-          )
+    if request.method == 'POST':
+        property = Property(
+            realtor_id=current_user.id,
+            title=request.form.get('title'),
+            description=request.form.get('description'),
+            location=request.form.get('location'),
+            price=float(request.form.get('price')),
+            property_type=request.form.get('property_type'),
+            area=float(request.form.get('area')),
+            bedrooms=int(request.form.get('bedrooms')),
+            bathrooms=int(request.form.get('bathrooms')),
+            status='available'
+        )
         
-          db.session.add(property)
-          db.session.commit()
+        db.session.add(property)
+        db.session.commit()
         
-          # Handle photo uploads
-          if 'photos' in request.files:
-              photos = request.files.getlist('photos')
-              for photo in photos:
-                  if photo.filename:
-                      filename = secure_filename(photo.filename)
-                      s3_url = upload_file_to_s3(photo, filename)
-                      if s3_url:
-                          property_photo = PropertyPhoto(
-                              property_id=property.id,
-                              photo_url=s3_url
-                          )
-                          db.session.add(property_photo)
+        if 'photos' in request.files:
+            photos = request.files.getlist('photos')
+            for photo in photos:
+                if photo.filename:
+                    s3_url = upload_file_to_s3(photo, S3_BUCKET)
+                    if s3_url:
+                        property_photo = PropertyPhoto(
+                            property_id=property.id,
+                            photo_url=s3_url
+                        )
+                        db.session.add(property_photo)
             
-              db.session.commit()
+            db.session.commit()
         
-          flash('Property added successfully!', 'success')
-          return redirect(url_for('properties'))
+        flash('Property added successfully!', 'success')
+        return redirect(url_for('properties'))
     
-      return render_template('property_form.html')
+    return render_template('property_form.html')
+
 @app.route('/properties/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_property(id):
@@ -150,19 +165,17 @@ def edit_property(id):
         property.bedrooms = int(request.form.get('bedrooms'))
         property.bathrooms = int(request.form.get('bathrooms'))
         
-        # Handle new photo uploads
         if 'photos' in request.files:
             photos = request.files.getlist('photos')
             for photo in photos:
                 if photo.filename:
-                    filename = secure_filename(photo.filename)
-                    photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    
-                    property_photo = PropertyPhoto(
-                        property_id=property.id,
-                        photo_url=f'/static/uploads/{filename}'
-                    )
-                    db.session.add(property_photo)
+                    s3_url = upload_file_to_s3(photo, S3_BUCKET)
+                    if s3_url:
+                        property_photo = PropertyPhoto(
+                            property_id=property.id,
+                            photo_url=s3_url
+                        )
+                        db.session.add(property_photo)
         
         db.session.commit()
         flash('Property updated successfully!', 'success')
@@ -173,5 +186,4 @@ def edit_property(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))    app.run(host='0.0.0.0', port=port)
