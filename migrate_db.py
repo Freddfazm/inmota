f29f54b1e0b265fb1b096a61d435fc5cf1db0d77
Freddy Zambrano <fredd.fazm@gmail.com>
2025-03-06 17:00:38 -0400

    fix migration script

--- migrate_db.py
+++ migrate_db.py
@@ -1,9 +1,10 @@
 from app import app, db
+from sqlalchemy import text
 
 def add_garage_spaces_column():
     with app.app_context():
-        # Execute raw SQL to add the column
-        db.session.execute('ALTER TABLE property ADD COLUMN garage_spaces INTEGER NOT NULL DEFAULT 0')
+        # Execute raw SQL to add the column, properly wrapped with text()
+        db.session.execute(text('ALTER TABLE property ADD COLUMN garage_spaces INTEGER NOT NULL DEFAULT 0'))
         db.session.commit()
         print("Successfully added garage_spaces column")
 
